47d9d32ea16e2ac283d06b500fcb8c904b37fed7
robson-resende <robsonrezende@outlook.com>
2022-10-12 10:37:17 -0300

    tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudar a classe String que representa texto na aplicação
+
+        String meuNome = "Robson Resende";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal1 = numeroCurto;
+        short numeroCurto2 = numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
     }
 }
